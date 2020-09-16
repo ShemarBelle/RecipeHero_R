a12e17f215c9031b46b92dc6ffbbc123ae225f9f
Shemar Brooks <shemarbb13@gmail.com>
2020-09-16 08:43:57 -0400

    Starting on the Save Function

--- RecipeHero/www/scripts/index.js
+++ RecipeHero/www/scripts/index.js
@@ -2,31 +2,21 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+let trigger=false;
 (function () {
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
+
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+    
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        trigger = true;
     
     };
-    function addToList() {
-        var txtToAdd = document.getElementById("enterIngredients").value,
-            listNode = document.getElementById("list"),
-            liNode = document.createElement("LI"),
-            txtNode = document.createTextNode(txtToAdd);
-        listNode.appendChild(txtNode);
-    }
+
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
     };
@@ -34,4 +24,7 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
+function GetTrigger() {
+    return trigger;
+}
